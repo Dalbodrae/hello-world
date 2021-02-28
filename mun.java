cccd5de3e5ace4185760b47d8c4ed9dada38fc71
dalbodare <jhk8500@g.yju.ac.kr>
2021-02-28 19:45:10 +0900

    숫자 문자 구별
    입력받은 것이 문자인지 숫자인지 구분

---
+++ mun.java
@@ -0,0 +1,30 @@
+package ch2;
+import java.util.Scanner;
+public class mun {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = ' ';
+		
+		System.out.printf("문자를 하나 입력하시오 : %n" );
+		
+		String str = sc.nextLine();
+		ch = str.charAt(0);
+		
+		if ('0' <= ch && ch <= '9') 
+		{
+			System.out.printf("입력하신 문자는 숫자입니다 %n");
+		}
+		
+		if ( ('a' <= ch && ch <= 'z') ||  ('A' <= ch && ch <= 'Z') ) 
+		{
+			System.out.printf("입력하신 문자는 문자입니다 %n");
+		}
+		
+		
+				
+	}
+
+}
